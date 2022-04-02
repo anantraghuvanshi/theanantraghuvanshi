6205ebc6f86eeaefc17e02c3484a3922839240a5
Anant <anantsinghraghuvanshi1@gmail.com>
2022-04-03 00:21:48 +0530

    favicon add

--- anant-portfolio/src/components/header/CTA.jsx
+++ anant-portfolio/src/components/header/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
   return (
     <div className='cta'>
         <a href={Resume} download className='btn' >Download Resume</a>
-        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        <a href="#contact" className='btn btn-primary'>Talk To Me</a>
     </div>
   )
 }
--- anant-portfolio/src/components/nav/nav.jsx
+++ anant-portfolio/src/components/nav/nav.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
-import './nav.css'
+import React, { useState } from "react";
+import "./nav.css";
+import { FiHome } from "react-icons/fi";
+import { FiUser } from "react-icons/fi";
+import { BiBook } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
+import { RiContactsBook2Line } from "react-icons/ri";
 
-const nav = () => {
+const Nav = () => {
+  const [activeNav, setactiveNav] = useState("#");
   return (
-    <div>nav</div>
-  )
-}
+    <nav>
+      <a
+        href="#"
+        onClick={() => setactiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <FiHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setactiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <FiUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setactiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <BiBook />
+      </a>
+      <a
+        href="#portfolio"
+        onClick={() => setactiveNav("#portfolio")}
+        className={activeNav === "#portfolio" ? "active" : ""}
+      >
+        <RiServiceLine />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setactiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <RiContactsBook2Line />
+      </a>
+    </nav>
+  );
+};
 
-export default nav
+export default Nav;
