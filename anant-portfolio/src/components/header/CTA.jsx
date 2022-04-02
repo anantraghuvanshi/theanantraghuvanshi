57d4e5da7467d63153aa1b018c9440578659d3f3
Anant <anantsinghraghuvanshi1@gmail.com>
2022-04-02 22:51:40 +0530

    cta add

---
+++ anant-portfolio/src/components/header/CTA.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Resume from '../../assets/Opensource-resume.pdf'
+
+const CTA = () => {
+  return (
+    <div className='cta'>
+        <a href={Resume} download className='btn' >Download Resume</a>
+        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+    </div>
+  )
+}
+
+export default CTA
--- anant-portfolio/src/components/header/header.jsx
+++ anant-portfolio/src/components/header/header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './header.css';
+import CTA from './CTA';
 
 const header = () => {
   return (
-    <div>header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello, People know me as</h5>
+        <h1>Anant Singh Raghuvanshi</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+      </div>
+    </header>
   )
 }
 
