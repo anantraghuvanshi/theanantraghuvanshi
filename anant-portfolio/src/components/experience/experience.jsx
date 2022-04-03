3563d78f72e52f0ae596dc238915be801f37df61
Anant <anantsinghraghuvanshi1@gmail.com>
2022-04-03 16:47:19 +0530

    experience.jsx

--- anant-portfolio/src/components/experience/experience.jsx
+++ anant-portfolio/src/components/experience/experience.jsx
@@ -3,7 +3,19 @@ import './experience.css';
 
 const experience = () => {
   return (
-    <section id='experience'>experience</section>
+    <section id='experience'>
+      <h5>Skill I have</h5>
+      <h2>Experience</h2>
+
+       <div className="container experience__container">
+         <div className="experience__frontend">
+
+         </div>
+         <div className="experience__backend">
+
+         </div>
+       </div>
+    </section> 
   )
 }
 
