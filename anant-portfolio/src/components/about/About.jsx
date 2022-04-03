bad0d20ee392d52ca1acc9bc3dd8497cb2a9e2ee
Anant <anantsinghraghuvanshi1@gmail.com>
2022-04-03 16:19:47 +0530

    about.css update

--- anant-portfolio/src/components/about/About.jsx
+++ anant-portfolio/src/components/about/About.jsx
@@ -12,37 +12,40 @@ const About = () => {
       <h2>About Anant</h2>
 
       <div className="container about__container">
-      <div className="about__me">
-        <div className="about__me-image">
-          <img src={ME} alt="Anant About" />
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="Anant About" />
+          </div>
         </div>
-      </div>
 
-      <div className="about__content">
-        <div className="about__cards">
-          <article className="about__card">
-            <FiAward className="about__icon" />
-            <h5>Experience</h5>
-            <small>Fresh College Graduate(2022)</small>
-          </article>
-          <article className="about__card">
-            <FiUsers className="about__icon" />
-            <h5>Tech Stack </h5>
-            <small>MERN Stack</small>
-          </article>
-          <article className="about__card">
-            <VscFolderLibrary className="about__icon" />
-            <h5>Projects</h5>
-            <small>15+ completed projects</small>
-          </article>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FiAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>Fresh College Graduate(2022)</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Tech Stack </h5>
+              <small>MERN Stack</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>15+ completed projects</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error
+            obcaecati placeat ipsa quisquam tempore explicabo doloribus, atque,
+            non et reprehenderit vero, minus mollitia consequuntur saepe illo ea
+            magni aspernatur? Et.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Talk To Anant
+          </a>
         </div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error obcaecati placeat ipsa quisquam tempore explicabo doloribus, 
-          atque, non et reprehenderit vero, 
-          minus mollitia consequuntur saepe illo ea magni aspernatur? Et.
-        </p>
-        <a href="#contact" className="btn btn-primary">Talk To Anant</a>
-      </div>
       </div>
     </section>
   );
