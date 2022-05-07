7ac84c7114036360f91aade5d56dbc183bcf35e8
Anant <anantsinghraghuvanshi1@gmail.com>
2022-05-07 21:01:12 +0530

    update

--- anant-portfolio/src/components/testimonials/testimonials.jsx
+++ anant-portfolio/src/components/testimonials/testimonials.jsx
@@ -2,6 +2,26 @@ import React from 'react'
 import './testimonials.css';
 import img1 from '../../assets/me.png';
 
+
+const data= [
+{
+avalar: img1,
+name: "Tina Saow",
+review: 'Mudi alias animi dolurem aliquam ea eum bealae maiures, conseclelur praesentiun quibusdam, commodi velit porru deserunt explicabo nostrum ducimus quasi?'
+},
+{
+avatar: img1,
+name: 'Shatta Wale',
+review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditlis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores descrunt explicabo nostrum ducimus quasi?'
+},
+{
+avatar: img1,
+name: 'Kwame Despite',
+review: 'Modi alias animi dolorcm aliquam ca cum pcatac maiorcs, consectetur pracsentium quibusdam, commodi velit porro o blanditiis conscquatur qui molestiac. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
+}
+
+];
+
 const testimonials = () => {
   return (
     <section id='testimonials'>
@@ -12,39 +32,18 @@ const testimonials = () => {
         Testimonials
       </h2>
       <div className="container testimonials__container">
-        <article className="testimonial">
+        {
+          data.map(({avatar, name, review}, index) =>{
+            <article key={index} className="testimonial">
           <div className="client__avatar">
-            <img src= {img1} alt="Avatar One" />
+            <img src= {img1} />
           </div>
-          <h5 className="client__review">Vipul Tripathi</h5>
-            <small className="collegue__review">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus nisi obcaecati 
-              commodi temporibus deserunt at eaque atque excepturi error eveniet soluta, facere, quia
-               sint amet dolorem voluptates enim placeat dolores?
+          <h5 className="client__review">{name}</h5>
+            <small className="collegue__review">{review}           
             </small>
         </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src= {img1} alt="Avatar One" />
-          </div>
-          <h5 className="client__review">Vipul Tripathi</h5>
-            <small className="collegue__review">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus nisi obcaecati 
-              commodi temporibus deserunt at eaque atque excepturi error eveniet soluta, facere, quia
-               sint amet dolorem voluptates enim placeat dolores?
-            </small>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src= {img1} alt="Avatar One" />
-          </div>
-          <h5 className="client__review">Vipul Tripathi</h5>
-            <small className="collegue__review">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus nisi obcaecati 
-              commodi temporibus deserunt at eaque atque excepturi error eveniet soluta, facere, quia
-               sint amet dolorem voluptates enim placeat dolores?
-            </small>
-        </article>
+          })
+        }
       </div>
     </section>
   )
