4337465d831f0311f7dedfcc15fa4f267ee98c75
Anant <anantsinghraghuvanshi1@gmail.com>
2022-05-07 20:41:53 +0530

    update

--- anant-portfolio/src/components/testimonials/testimonials.jsx
+++ anant-portfolio/src/components/testimonials/testimonials.jsx
@@ -23,6 +23,28 @@ const testimonials = () => {
                sint amet dolorem voluptates enim placeat dolores?
             </small>
         </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src= {img1} alt="Avatar One" />
+          </div>
+          <h5 className="client__review">Vipul Tripathi</h5>
+            <small className="collegue__review">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus nisi obcaecati 
+              commodi temporibus deserunt at eaque atque excepturi error eveniet soluta, facere, quia
+               sint amet dolorem voluptates enim placeat dolores?
+            </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src= {img1} alt="Avatar One" />
+          </div>
+          <h5 className="client__review">Vipul Tripathi</h5>
+            <small className="collegue__review">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus nisi obcaecati 
+              commodi temporibus deserunt at eaque atque excepturi error eveniet soluta, facere, quia
+               sint amet dolorem voluptates enim placeat dolores?
+            </small>
+        </article>
       </div>
     </section>
   )
