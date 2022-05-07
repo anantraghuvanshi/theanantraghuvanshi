f180fa3fc8b7daf9ef4f1b5522c184e0a3760d83
Anant <anantsinghraghuvanshi1@gmail.com>
2022-05-07 20:28:36 +0530

    testimonial update

--- anant-portfolio/src/components/testimonials/testimonials.jsx
+++ anant-portfolio/src/components/testimonials/testimonials.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './testimonials.css';
+import img1 from '../../assets/me.png';
 
 const testimonials = () => {
   return (
@@ -10,7 +11,13 @@ const testimonials = () => {
       <h2>
         Testimonials
       </h2>
-      
+      <div className="container testimonials__container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src="" alt="" />
+          </div>
+        </article>
+      </div>
     </section>
   )
 }
