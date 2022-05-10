6c3f0c330efa5d3d3435b374ba51cd3b5254c6a1
Anant <anantsinghraghuvanshi1@gmail.com>
2022-05-10 20:30:53 +0530

    file update

--- anant-portfolio/src/components/contact/contact.jsx
+++ anant-portfolio/src/components/contact/contact.jsx
@@ -3,7 +3,21 @@ import './contact.css';
 
 const contact = () => {
   return (
-    <section id='contact'>contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <h4>Email</h4>
+            <h5>anantsinghraghuvanshi1@gmail.com</h5>
+            <a href="mailto:anantsinghraghuvanshi1@gmail.com">Drop a Mail</a>
+          </article>
+        </div>
+        <form action=""></form>
+      </div>
+    </section>
   )
 }
 
