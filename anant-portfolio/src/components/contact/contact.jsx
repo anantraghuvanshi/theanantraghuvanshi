45ea5296c05edb9efad7a3932b49eea9711a50c7
Anant <anantsinghraghuvanshi1@gmail.com>
2022-05-10 21:02:17 +0530

    contact.js update

--- anant-portfolio/src/components/contact/contact.jsx
+++ anant-portfolio/src/components/contact/contact.jsx
@@ -32,7 +32,7 @@ const contact = () => {
           <input type="text" name="name" placeholder="Your Full Name" required/>
           <input type="email" name="email" placeholder="Your Email" required/>
           <textarea name="message" placeholder="Type Your Message Here"  rows="7" required></textarea>
-
+          <button type="submit" className="btn btn-primary">Send Message</button>
         </form>
       </div>
     </section>
