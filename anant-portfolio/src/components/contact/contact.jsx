6fda87f5663fb20557a552e1de3f2f7e2080913a
Anant <anantsinghraghuvanshi1@gmail.com>
2022-05-10 21:01:09 +0530

    form add

--- anant-portfolio/src/components/contact/contact.jsx
+++ anant-portfolio/src/components/contact/contact.jsx
@@ -1,33 +1,42 @@
-import React from 'react'
-import './contact.css';
-import {HiOutlineMail} from 'react-icons/hi';
-import {FaTelegramPlane} from 'react-icons/fa';
+import React from "react";
+import "./contact.css";
+import { HiOutlineMail } from "react-icons/hi";
+import { FaTelegramPlane } from "react-icons/fa";
 
 const contact = () => {
   return (
-    <section id='contact'>
+    <section id="contact">
       <h5>Get In Touch</h5>
       <h2>Contact me</h2>
 
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <HiOutlineMail />
-            <h4>Email</h4>
-            <h5>anantsinghraghuvanshi1@gmail.com</h5>
-            <a href="https://t.me/sundi0110">Drop a Mail</a>
-          </article>
-          <article className="contact__option">
             <FaTelegramPlane />
             <h4>Telegram</h4>
             <h5>Anant Singh Raghuvanshi</h5>
-            <a href="mailto:anantsinghraghuvanshi1@gmail.com">Say Hi on Telegram</a>
+            <a href="mailto:anantsinghraghuvanshi1@gmail.com" target="_blank">
+              Say Hi on Telegram
+            </a>
+          </article>
+          <article className="contact__option">
+            <HiOutlineMail />
+            <h4>Email</h4>
+            <h5>anantsinghraghuvanshi1@gmail.com</h5>
+            <a href="https://t.me/sundi0110" target="_blank">
+              Drop a Mail
+            </a>
           </article>
         </div>
-        <form action=""></form>
+        <form action="">
+          <input type="text" name="name" placeholder="Your Full Name" required/>
+          <input type="email" name="email" placeholder="Your Email" required/>
+          <textarea name="message" placeholder="Type Your Message Here"  rows="7" required></textarea>
+
+        </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default contact
+export default contact;
