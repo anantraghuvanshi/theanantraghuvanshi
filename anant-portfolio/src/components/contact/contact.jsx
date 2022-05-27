2d8e5d3369b01c2f415ce74288a080e99cdeaa96
Anant <anantsinghraghuvanshi1@gmail.com>
2022-05-27 21:22:26 +0530

    email form fixed

--- anant-portfolio/src/components/contact/contact.jsx
+++ anant-portfolio/src/components/contact/contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_hfh1tet', 'template_j6fvnkd', form.current, 'YOUR_PUBLIC_KEY')
+    emailjs.sendForm('service_hfh1tet', 'template_j6fvnkd', form.current, 'LH1Va_XLcurogrm6Q')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
